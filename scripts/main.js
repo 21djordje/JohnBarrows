bebea99bff15c647d2619943cf4355c2816a63c4
djordje.sinikovic <djordjesinikovic@gmail.com>
2022-06-01 02:26:16 +0200

    John, first version

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
+    type: "iframe",
+    mainClass: "mfp-fade",
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false,
+  });
+});
